0ac991d18c40e3a9c11257ef378a225b89ab4dac
mat3usq <mmogielnicki123@gmail.com>
2024-04-24 00:26:37 +0200

    auditing works

--- src/main/java/com/driving/school/model/SchoolUser.java
+++ src/main/java/com/driving/school/model/SchoolUser.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "SCHOOLUSER")
 public class SchoolUser {
     @Id
