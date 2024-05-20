48d672ff08e3ace4ce3f59e19774f1fefbacbbe5
mat3usq <mmogielnicki123@gmail.com>
2024-05-20 23:37:18 +0200

    update calendar view

--- src/main/resources/static/assets/js/calendar.js
+++ src/main/resources/static/assets/js/calendar.js
@@ -2,7 +2,10 @@ const calendar = document.querySelector('.calendar'),
     date = document.querySelector('.date'),
     daysContainer = document.querySelector('.days'),
     prev = document.querySelector('.prev'),
-    next = document.querySelector('.next')
+    next = document.querySelector('.next'),
+    todayBtn = document.querySelector('.today-btn'),
+    gotoBtn = document.querySelector('.goto-btn'),
+    dateInput = document.querySelector('.date-input')
 
 let today = new Date()
 let activeDay
@@ -32,7 +35,7 @@ function initCalendar() {
     const lastDate = lastDay.getDate()
 
     let day = firstDay.getDay()
-    day = (day === 0) ? 6 : day - 1
+    day = day === 0 ? 6 : day - 1
 
     const nextDays = 7 - lastDay.getDay()
 
@@ -57,3 +60,65 @@ function initCalendar() {
 }
 
 initCalendar()
+
+//function to add month and year on prev and next button
+function prevMonth() {
+    month--
+    if (month < 0) {
+        month = 11
+        year--
+    }
+    initCalendar()
+}
+
+function nextMonth() {
+    month++
+    if (month > 11) {
+        month = 0
+        year++
+    }
+    initCalendar()
+}
+
+prev.addEventListener('click', prevMonth)
+next.addEventListener('click', nextMonth)
+
+todayBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+dateInput.addEventListener("input", (e) => {
+    dateInput.value = dateInput.value.replace(/[^0-9]/g, "");
+
+    if (dateInput.value.length === 2 && !dateInput.value.includes("/"))
+        dateInput.value += "/";
+
+    if (dateInput.value.length > 7)
+        dateInput.value = dateInput.value.slice(0, 7);
+
+    if (e.inputType === "deleteContentBackward")
+        if (dateInput.value.length === 3 && dateInput.value.includes("/"))
+            dateInput.value = dateInput.value.slice(0, 2);
+});
+
+dateInput.addEventListener("keydown", (e) => {
+    if (e.key >= '0' && e.key <= '9' && dateInput.value.length === 2 && !dateInput.value.includes("/"))
+        dateInput.value += "/";
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+    const dateArr = dateInput.value.split("/");
+    if (dateArr.length === 2)
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    alert("Nieprawidłowa Data");
+}
