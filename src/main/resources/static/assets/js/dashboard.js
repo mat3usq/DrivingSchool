bec16aab1b0d2dabfaf940323d6739c9148d1ccb
mat3usq <mmogielnicki123@gmail.com>
2024-10-25 20:17:13 +0200

    update

--- src/main/resources/static/assets/js/dashboard.js
+++ src/main/resources/static/assets/js/dashboard.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const rowsInstructor = document.querySelectorAll('.table-instructor-row');
-    rowsInstructor.forEach(row => {
-        row.addEventListener('click', () => {
-            row.classList.toggle('clicked');
+    const threeOptions = document.querySelectorAll('.table-three-options');
+    threeOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            option.classList.toggle('clicked');
         });
     });
 
-    const rowsStudent = document.querySelectorAll('.table-student-row');
-    rowsStudent.forEach(row => {
-        row.addEventListener('click', () => {
-            row.classList.toggle('clicked');
+    const twoOptions = document.querySelectorAll('.table-two-options');
+    twoOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            option.classList.toggle('clicked');
+        });
+    });
+
+    const oneOption = document.querySelectorAll('.table-one-option');
+    oneOption.forEach(option => {
+        option.addEventListener('click', () => {
+            option.classList.toggle('clicked');
         });
     });
 });
