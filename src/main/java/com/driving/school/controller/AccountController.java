e73eefd5c251f1f925ab42308b449a71b3f86e00
mat3usq <mmogielnicki123@gmail.com>
2024-10-27 17:50:45 +0100

    student can see payments in account view

--- src/main/java/com/driving/school/controller/AccountController.java
+++ src/main/java/com/driving/school/controller/AccountController.java
@@ -1,13 +1,30 @@
 package com.driving.school.controller;
 
+import com.driving.school.model.SchoolUser;
+import com.driving.school.repository.SchoolUserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AccountController {
+    private final SchoolUserRepository schoolUserRepository;
+
+    @Autowired
+    public AccountController(SchoolUserRepository schoolUserRepository) {
+        this.schoolUserRepository = schoolUserRepository;
+    }
+
     @GetMapping("/account")
-    public ModelAndView displayAccount() {
-        return new ModelAndView("account");
+    public ModelAndView displayAccount(HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView("account");
+        SchoolUser user = schoolUserRepository.findByEmail(((SchoolUser) session.getAttribute("loggedInUser")).getEmail());
+        if (user != null) {
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+        return new ModelAndView("redirect:/dashboard");
     }
 }
