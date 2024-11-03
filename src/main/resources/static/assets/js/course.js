f21bb5764a035171391f4223245dbcebacab8832
mat3usq <mmogielnicki123@gmail.com>
2024-11-03 17:27:30 +0100

    display testcourse in front

--- src/main/resources/static/assets/js/course.js
+++ src/main/resources/static/assets/js/course.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const options = document.querySelectorAll('.drivingSession');
+    const drivingSessions = document.querySelectorAll('.drivingSession');
+    const testCourses = document.querySelectorAll('.testCourse');
+
+    const options = [...drivingSessions, ...testCourses];
+
     options.forEach(option => {
         option.addEventListener('click', () => {
             option.classList.toggle('clicked');
